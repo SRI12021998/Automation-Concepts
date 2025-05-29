b5b784d1786896365e8db424ee9081c0e14ae6ba
sriram.b <sriram.b@LT-LZ-0230.ivygdcad.ivy-support.com>
2025-05-29 12:23:42 +0530

    Added javascriptexecutor program

---
+++ 3.0/src/test/java/javascriptexecutor/JavascriptExecutorDemo.java
@@ -0,0 +1,55 @@
+package javascriptexecutor;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+public class JavascriptExecutorDemo 
+{
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.nopcommerce.com/login?returnUrl=%2Fjewelry");
+		driver.manage().window().maximize();
+		Actions action=new Actions(driver);
+		//space should not be there if locator is class not applicable for xpath
+		action.moveToElement(driver.findElement(By.className("register-button"))).click().perform();
+		LinkedHashMap <By,String> field=new LinkedHashMap<>();
+		field.put(By.id("gender-male"), null);
+		field.put(By.name("FirstName"), "Sriram");
+		field.put(By.name("LastName"), "Balaji");
+		field.put(By.id("Email"), "s781@gmail.com");
+		field.put(By.id("Company"), "IVY MOBILITY");
+		field.put(By.id("Newsletter"), null);
+		field.put(By.id("Password"), "drunkenmonkey");
+		field.put(By.id("ConfirmPassword"), "drunkenmonkey");
+		field.put(By.xpath("//button[@id='register-button']"), null);
+		JavascriptExecutor je=(JavascriptExecutor)driver;
+		for(Entry<By, String> job:field.entrySet())
+		{
+			WebElement element=driver.findElement(job.getKey());
+			je.executeScript("arguments[0].scrollIntoView(true)", element);
+			if(job.getValue()==null)
+				element.click();
+			else
+				element.sendKeys(job.getValue());
+		}
+		Thread.sleep(3000);
+		try 
+		{
+			System.out.println(driver.findElement(By.xpath("//div[@class='result']")).getText());
+		}
+		catch (Exception e) 
+		{
+			System.out.println(driver.findElement(By.xpath("//li[text()='The specified email already exists']")).getText());
+		}
+		driver.manage().window().minimize();
+		driver.quit();
+	}
+
+}
