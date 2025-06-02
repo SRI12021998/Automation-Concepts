9818d4b5ceabc974e8bb0fe8cd8a743825bf2494
sriram.b <sriram.b@LT-LZ-0230.ivygdcad.ivy-support.com>
2025-06-02 22:10:53 +0530

    Loan approval program and sales order optimization

--- 3.0/src/test/java/AutomationTest/SalesOrderCreate.java
+++ 3.0/src/test/java/AutomationTest/SalesOrderCreate.java
@@ -4,13 +4,17 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class SalesOrderCreate 
 {
@@ -25,7 +29,7 @@ public class SalesOrderCreate
 		LocalDateTime dt=LocalDateTime.now();
 		DateTimeFormatter yearFormat=DateTimeFormatter.ofPattern("YYYY");
 		DateTimeFormatter monthFormat=DateTimeFormatter.ofPattern("MMM", Locale.ENGLISH);
-		DateTimeFormatter dateFormat=DateTimeFormatter.ofPattern("dd");
+		DateTimeFormatter dateFormat=DateTimeFormatter.ofPattern("d");
 		
 		String year=dt.format(yearFormat);
 		String month=dt.format(monthFormat);
@@ -48,7 +52,7 @@ public class SalesOrderCreate
 		action.moveToElement(element).click().perform();
 		
 		//wait and switch to i content
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("iContent")));
 		
 		//wait and enter sales person 
@@ -56,9 +60,10 @@ public class SalesOrderCreate
 		driver.findElement(By.id("SalesPerson")).click();//click
 		driver.findElement(By.id("SalesPerson")).sendKeys("014214_3000");//input
 		
+		
 		//wait for suggestion to load
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@tabindex='-1']")));
+		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//ul[@id='ui-id-1']/li[@class='ui-menu-item']"), 0));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//ul[@id='ui-id-1']/li/a)[1]")));
 		
 		//go down and enter
 		driver.findElement(By.id("SalesPerson")).sendKeys(Keys.ARROW_DOWN);
@@ -70,15 +75,16 @@ public class SalesOrderCreate
 		driver.findElement(By.xpath("//input[@id='Retailer']")).sendKeys("DEMORET1");//input
 		
 		//wait for suggestion to load
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[.='DEMORET1']")));
+		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//ul[@id='ui-id-5']/li[@class='ui-menu-item']"), 0));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//ul[@id='ui-id-5']/li/a)[1]")));
 		
 		//go down and enter
 		driver.findElement(By.xpath("//input[@id='Retailer']")).sendKeys(Keys.ARROW_DOWN);
 		driver.findElement(By.xpath("//input[@id='Retailer']")).sendKeys(Keys.ENTER);
 		
-		//select the delivery date
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='DeliveryDate']")));
+		
+		//wait and select the delivery date
+		
 		driver.findElement(By.xpath("//input[@id='DeliveryDate']")).click();
 		driver.findElement(By.xpath("//select[@class='ui-datepicker-year']/option[.='"+year+"']")).click();
 		driver.findElement(By.xpath("//select[@class='ui-datepicker-month']/option[.='"+month+"']")).click();
---
+++ 3.0/src/test/java/rough/LoanApproval.java
@@ -0,0 +1,99 @@
+package rough;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import java.time.Duration;
+import java.util.Scanner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class LoanApproval 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter username");
+		String userName=scn.next();
+		System.out.println("Enter password");
+		String Password=scn.next();
+		System.out.println("Enter pacs name correctly in uppercase");
+		String pacsName=scn.next();
+		System.out.println("Starting Auto approval please wait...");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://fasalrin.gov.in/login");
+		driver.manage().window().maximize();
+		
+		//login using credentials
+		driver.findElement(By.name("username")).sendKeys(userName);
+		driver.findElement(By.name("loginPwd")).sendKeys(Password);
+		
+		//wait for captcha & click login
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//button[@title='Login']")).click();
+		
+		//wait and close window alert
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.cssSelector("button[class$='alertBodyModalBtn']")).click();
+		
+		//open dashboard menu
+		driver.findElement(By.xpath("(//a[@title='Dashboard'])[1]")).click();
+		
+		//click on view details of pending loan
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//p[contains(.,'Applications pending')]/following-sibling::button")).click();
+		
+		//select branch/pacs dropown
+		Select pacs=new Select(driver.findElement(By.name("branchOrPacs")));
+		pacs.selectByVisibleText("PACS");
+		
+		//select Pacs
+		Select pacsList=new Select(driver.findElement(By.name("pacsList")));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		pacsList.selectByVisibleText(pacsName);
+		
+		//click on proceed
+		driver.findElement(By.xpath("//button/span[.='PROCEED']")).click();
+		
+		//store the no of records
+		String temp=driver.findElement(By.xpath("//div[contains(.,'Total Count:')]/b")).getText();
+		int records=Integer.parseInt(temp);
+		
+		//move to review button
+		for (int i=0;i<records;i++)
+		{
+			//move to review button
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			WebElement review=driver.findElement(By.xpath("//div[@class='loanApplicationTable']/descendant::tbody/tr[1]/td[8]/div/button"));
+			Actions action=new Actions(driver);
+			action.moveToElement(review).perform();
+			
+			//click on review button
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			review.click();
+			
+			//move to approve button
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			WebElement approve=driver.findElement(By.xpath("//button/span[.='APPROVE']"));
+			action.moveToElement(approve).perform();
+			
+			//click on approve button
+			approve.click();
+			
+			//click on confirm
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.findElement(By.xpath("//button[.='CONFIRM']")).click();
+		
+			//click on ok
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.findElement(By.xpath("//button[.='OK']")).click();
+		}
+		driver.findElement(By.xpath("(//span[.='Logout'])[1]")).click();
+		Thread.sleep(2000);
+		System.out.println("Operation completed");
+		driver.quit();
+	}
+}
